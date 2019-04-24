62bc8b2dab9f7ca3d0af4de989af4b5ecc00bd1a
Hamid Khalid <hamidkhalid28@gmail.com>
2019-04-24 23:30:46 +0200

    Unity Scripts Demo - Initial Commit
    Added for Demo

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public GameObject player;
+
+	private Vector3 offset;
+
+	bool zoomin = false;
+
+	Vector3 original_offset;
+
+	Vector3 newOffset;
+
+	bool setout = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - player.transform.position;
+		original_offset = offset;
+		newOffset = original_offset - new Vector3 (original_offset.x - 5f, original_offset.y - 5f, original_offset.z - 5f);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		if(zoomin && !setout)
+		{
+			offset = Vector3.Lerp (offset, newOffset, Time.deltaTime);
+		}
+		else if(setout && !zoomin)
+		{
+			offset = Vector3.Lerp (offset, original_offset, Time.deltaTime);
+		}
+
+		transform.position = player.transform.position + offset;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag(Tags.Boundary))
+		{
+			setout = false;
+			zoomin = true;
+			Invoke ("setSetOut", 2);
+		}
+
+	}
+
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.CompareTag(Tags.Boundary))
+		{
+			zoomin = false;
+		}
+	}
+
+	void setSetOut()
+	{
+		setout = true;
+	}
+
+	bool CompareVectors(Vector3 a ,Vector3 b, float angleError) 
+	{
+		//if they aren't the same length, don't bother checking the rest.
+		if(!Mathf.Approximately(a.magnitude, b.magnitude))
+			return false;
+		float cosAngleError = Mathf.Cos(angleError * Mathf.Deg2Rad);
+		//A value between -1 and 1 corresponding to the angle.
+		var cosAngle = Vector3.Dot(a.normalized, b.normalized);
+		//The dot product of normalized Vectors is equal to the cosine of the angle between them.
+		//So the closer they are, the closer the value will be to 1.  Opposite Vectors will be -1
+		//and orthogonal Vectors will be 0.
+
+		if(cosAngle >= cosAngleError) {
+			//If angle is greater, that means that the angle between the two vectors is less than the error allowed.
+			return true;
+		}
+		else 
+			return false;
+	}
+}
---
+++ Scripts/PlayerSelectionMenuController.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using com.aeksaekhow.androidnativeplugin;
+using GameAnalyticsSDK;
+
+[System.Serializable]
+public class PlayerData
+{
+	public Text coinsText;
+	public Text player_name;
+	public Image coinsImage;
+}
+
+public class PlayerSelectionMenuController : MonoBehaviour 
+{
+	public Button unlockBtn;
+	private PlayerTextureController textureController;
+	private PlayerEffectController effectController;
+
+	public PlayerData[] data;
+
+	public RectTransform playerSelectionPanel;
+	public RectTransform playBtn;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		LeanTween.move(playerSelectionPanel,Vector3.zero,1f).setEase(LeanTweenType.easeOutQuint);
+		LeanTween.scale(playBtn,new Vector3(1f,1f,1f),1).setDelay(2.6f).setLoopPingPong();
+
+		unlockBtn.interactable = false;
+		effectController = GameObject.FindGameObjectWithTag(Tags.player).GetComponent<PlayerEffectController>();
+		textureController = GameObject.FindGameObjectWithTag(Tags.player).GetComponentInChildren<PlayerTextureController>();
+		textureController.changeTexture();
+		effectController.showEffect();
+		setUnlockButton();
+		setSelectedText();
+		setCoinsUI();
+	}
+
+	public void onPlayerBtnClick(int playerNum)
+	{
+		GameManager.Instance.soundState.playSound(SoundController.States.SELECTIONCLICK);
+
+		Prefs.currentPlayer = playerNum;
+		textureController.changeTexture();
+		effectController.showEffect();
+		setUnlockButton();
+		setSelectedText();
+		setCoinsUI();
+	}
+
+	public void onPlayBtnClick()
+	{
+		if (Prefs.playerUnlockArray [Prefs.currentPlayer]) 
+		{
+			GameManager.Instance.soundState.stopBGMusic ();
+			GameManager.Instance.soundState.playSound (SoundController.States.LOADING);
+			GameManager.Instance.setGameState (GameState.States.SwtichScene);
+		}
+		else
+		{
+			GameManager.Instance.setGameState(GameState.States.LockedPlayer);
+		}
+	}
+
+	public void onUnlockBtnClick()
+	{
+		GameManager.Instance.soundState.playSound(SoundController.States.BTNCLICKSOUND);
+
+		if(Prefs.coins >= Constants.PlayerPrices[Prefs.currentPlayer])
+			GameManager.Instance.setGameState(GameState.States.PurchaseConfirmation);
+		else
+			GameManager.Instance.setGameState(GameState.States.OutOfCoins);
+
+	}
+
+	public void setUnlockButton()
+	{
+		if(!Prefs.playerUnlockArray[Prefs.currentPlayer])
+		{
+			unlockBtn.interactable = true;
+		}
+		else
+		{
+			unlockBtn.interactable = false;
+		}
+	}
+
+	public void onFreeCoinsBtnClick()
+	{
+		if(Application.platform.Equals(RuntimePlatform.Android))
+		{
+			if(AndroidNativePlugin.IsInternetConnected() || AndroidNativePlugin.IsWifiConnected() || AndroidNativePlugin.IsMobileConnected())
+			{
+				GameManager.Instance.setGameState(GameState.States.VideoAdConfirmation);
+				GameManager.Instance.soundState.playSound(SoundController.States.BTNCLICKSOUND);
+			}
+			else
+			{
+				GameManager.Instance.setGameState(GameState.States.InternetNotConnected);
+				GameManager.Instance.soundState.playSound(SoundController.States.BTNCLICKSOUND);
+
+			}
+		}
+		else
+		{
+			GameAnalytics.NewDesignEvent ("Menu:Free Coins Button Click");
+
+			GameManager.Instance.setGameState(GameState.States.VideoAdConfirmation);
+			GameManager.Instance.soundState.playSound(SoundController.States.BTNCLICKSOUND);
+		}
+	}
+
+	public void onBackBtnClick()
+	{
+		GameManager.Instance.soundState.playSound(SoundController.States.BTNCLICKSOUND);
+		GameManager.Instance.setGameState(GameState.States.MainMenu);
+
+	}
+
+	public void setSelectedText()
+	{
+
+		for(int i = 0;i < data.Length;i++)
+		{
+			if(i == Prefs.currentPlayer)
+			{
+				data [i].player_name.color = Color.red;
+
+			}
+			else
+			{
+				data [i].player_name.color = Color.black;
+
+			}
+
+		}
+
+	}
+
+	public void setCoinsUI()
+	{
+		for(int i = 0;i < Prefs.playerUnlockArray.Length;i++)
+		{
+			if(Prefs.playerUnlockArray[i])
+			{
+				data[i].coinsText.enabled = false;
+				data[i].coinsImage.enabled = false;
+			}
+		}
+	}
+}
